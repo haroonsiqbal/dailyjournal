3b2d7238da9f41f11309fe4338603d3c90009d51
Haroon Iqbal <harooniqbal@Haroons-MacBook-Pro.local>
2019-07-24 11:14:50 -0500

    converted to json

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -3,48 +3,48 @@
     represents a journal entry about what you learned today
 */
 
-const journalEntries = [
-{
-    dateOfEntry: "7/14/2019",
-    conceptsCovered: "Anonymous Functions",
-    journalEntry: "Today I learned that a function that is not named is called an anonymous function, which is also sometimes referred to as a lambda function.",
-    moodForTheDay: "Happy" 
-},
+// const journalEntries = [
+// {
+//     dateOfEntry: "7/14/2019",
+//     conceptsCovered: "Anonymous Functions",
+//     journalEntry: "Today I learned that a function that is not named is called an anonymous function, which is also sometimes referred to as a lambda function.",
+//     moodForTheDay: "Happy" 
+// },
 
-{
-    dateOfEntry: "7/15/2019",
-    conceptsCovered: "Arrow Functions",
-    journalEntry: "Today I learned that you do not need parantheses around a parameter in an arrow function if there is only parameter in the function.",
-    moodForTheDay: "Excited" 
-},
+// {
+//     dateOfEntry: "7/15/2019",
+//     conceptsCovered: "Arrow Functions",
+//     journalEntry: "Today I learned that you do not need parantheses around a parameter in an arrow function if there is only parameter in the function.",
+//     moodForTheDay: "Excited" 
+// },
 
-{
-    dateOfEntry: "7/16/2019",
-    conceptsCovered: "Clever Code",
-    journalEntry: "Today I learned that readable code is preferrable to clever code, even if it takes longer to write out and is less efficient.",
-    moodForTheDay: "Happy" 
-},
+// {
+//     dateOfEntry: "7/16/2019",
+//     conceptsCovered: "Clever Code",
+//     journalEntry: "Today I learned that readable code is preferrable to clever code, even if it takes longer to write out and is less efficient.",
+//     moodForTheDay: "Happy" 
+// },
 
-{
-    dateOfEntry: "7/17/2019",
-    conceptsCovered: "Dry Coding",
-    journalEntry: "Today I learned about the acronym DRY (Don't Repeat Yourself), which means you should avoid repeating lines of codes.",
-    moodForTheDay: "Excited"
-},
-{
-    dateOfEntry: "7/18/2019",
-    conceptsCovered: "Factory Functions",
-    journalEntry: "Today I learned about factory functions, which are functions that start with the word create and are generally used to create something.",
-    moodForTheDay: "Happy"
-},
-{
-    dateOfEntry: "7/19/2019",
-    conceptsCovered: "Postman Commands",
-    journalEntry: "Today I learned about the four most common commands in Postman (a program for working with JSON server data): GET, POST, DELETE, and PUT.",
-    moodForTheDay: "Anxious"
-}
+// {
+//     dateOfEntry: "7/17/2019",
+//     conceptsCovered: "Dry Coding",
+//     journalEntry: "Today I learned about the acronym DRY (Don't Repeat Yourself), which means you should avoid repeating lines of codes.",
+//     moodForTheDay: "Excited"
+// },
+// {
+//     dateOfEntry: "7/18/2019",
+//     conceptsCovered: "Factory Functions",
+//     journalEntry: "Today I learned about factory functions, which are functions that start with the word create and are generally used to create something.",
+//     moodForTheDay: "Happy"
+// },
+// {
+//     dateOfEntry: "7/19/2019",
+//     conceptsCovered: "Postman Commands",
+//     journalEntry: "Today I learned about the four most common commands in Postman (a program for working with JSON server data): GET, POST, DELETE, and PUT.",
+//     moodForTheDay: "Anxious"
+// }
 
-]
+// ]
 
 /*
     Purpose: To create, and return, a string template that
@@ -53,10 +53,17 @@ const journalEntries = [
     Arguments: journalEntry (object)
 */
 
+
+
 containerJournal = document.querySelector(".entryLog");
 
 const renderJournalEntries = () => {
-for (const journal of journalEntries) {
+
+fetch("http://localhost:3000/entries") // Fetch from the API
+    .then(data => data.json())  // Parse as JSON
+    .then(entries => {
+        // What should happen when we finally have the array?
+for (const journal of entries) {
   let makeJournalEntryComponent = `
         <article class="entryStyle">
           <div class="divStyle">
@@ -77,5 +84,6 @@ for (const journal of journalEntries) {
     containerJournal.innerHTML += makeJournalEntryComponent;
   }
 }
+)}
 // Invoke the render function
-renderJournalEntries(journalEntries);
+renderJournalEntries();
